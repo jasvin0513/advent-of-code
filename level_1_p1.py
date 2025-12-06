14a6b706cfc2f6885e03cb80380a489d425379ab
jasvin0513 <jasvin0513@gmail.com>
2025-12-05 20:42:00 -0500

    Completed part 1 and 2 of level 1

--- level_1_p1.py
+++ level_1_p1.py
@@ -1,37 +1,31 @@
-def countZeroDials(input: str)  -> int:
-    dialPosition = 50
-    turns = input.splitlines()
-    zeroCount = 0
+def count_zero_dials(input_data: str)  -> int:
+    dial_position = 50
+    turns = input_data.splitlines()
+    zero_count = 0
 
     for turn in turns:
-        dialPosition = turnDial(dialPosition, turn)
-        if dialPosition == 0:
-            zeroCount += 1
+        dial_position = turn_dial(dial_position, turn)
+        if dial_position == 0:
+            zero_count += 1
 
-    return zeroCount
+    return zero_count
             
-def turnDial(dialPosition: int, turn: str) -> int:
-    turnDirection = turn[0]
-    turnCount = int(turn[1:])
+def turn_dial(dial_position: int, turn: str) -> int:
+    turn_direction = turn[0]
+    turn_count = int(turn[1:])
     
-    # If we turn left, subtract the turnCount from the dial position
-    if turnDirection == "L":
-        dialPosition -= turnCount
-        # Handle underflow by looping back to 99
-        while (dialPosition < 0):
-            dialPosition += 100
-    # If we turn right, add the turnCount to the dial position
-    elif turnDirection == "R":
-        dialPosition += turnCount
-        # Handle overflow by looping back to 0
-        while (dialPosition > 99):
-            dialPosition -= 100
+    # If we turn left, subtract the turn_count from the dial position
+    if turn_direction == "L":
+        dial_position -= turn_count
+    # If we turn right, add the turn_count to the dial position
+    elif turn_direction == "R":
+        dial_position += turn_count
 
-    return dialPosition
+    return dial_position % 100
 
 
 if __name__ == "__main__":
     input_filename = 'level_1_input.txt'
     with open(input_filename, 'r') as file:
         file_content = file.read()
-        print(countZeroDials(file_content))
+        print(count_zero_dials(file_content))
---
+++ level_1_p2.py
@@ -0,0 +1,51 @@
+def count_zero_dials(input: str)  -> int:
+    dial_position = 50
+    turns = input.splitlines()
+    zero_count = 0
+
+    for turn in turns:
+        dial_position, zeros_hit = turn_dial(dial_position, turn)
+        zero_count += zeros_hit
+
+    return zero_count
+            
+def turn_dial(dial_position: int, turn: str) -> tuple[int, int]:
+    turn_direction = turn[0]
+    turn_count = int(turn[1:])
+    zeros_passed = 0
+
+    # If we start at 0, the next 0 is 100 clicks away
+    if dial_position == 0:
+        dist_to_first_zero = 100
+        if turn_direction == "L":
+            total_change = -turn_count
+        elif turn_direction == "R":
+            total_change = turn_count
+    # If we turn left, subtract the turn_count from the dial position
+    elif turn_direction == "L":
+        # Find the distance to the next zero going left
+        dist_to_first_zero = dial_position
+        total_change = -turn_count
+    # If we turn right, add the turn_count to the dial position
+    elif turn_direction == "R":
+        # Find the distance to the next zero going right
+        dist_to_first_zero = 100 - dial_position
+        total_change = turn_count
+    
+    # Calculate how much more to turn the dial if dial reaches the first zero  
+    remaining_turn = turn_count - dist_to_first_zero
+    # If the remaining turn is positive, the dial passes 0
+    if remaining_turn >= 0:
+        zeros_passed += 1
+        zeros_passed += remaining_turn // 100
+
+    final_position = (dial_position + total_change) % 100
+
+    return (final_position, zeros_passed)
+
+
+if __name__ == "__main__":
+    input_filename = 'level_1_input.txt'
+    with open(input_filename, 'r') as file:
+        file_content = file.read()
+        print(count_zero_dials(file_content))
